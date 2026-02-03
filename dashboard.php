611ebfd2ba9cda1e4766372b31376d4f853fdc2d
Mahamat Adam Mahamat <mamifsadam@gmail.com>
2026-02-03 15:06:14 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION["admin"])) {
+  header("Location: login.php");
+  exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dashboard</title>
+</head>
+<body>
+<h1>Welcome Admin ✅</h1>
+<a href="logout.php">Logout</a>
+</body>
+</html>
 
