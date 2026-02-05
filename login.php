2d23002e474fbb57b493c7a546b1253f9ab212c9
Mahamat Adam Mahamat <mamifsadam@gmail.com>
2026-02-05 14:45:50 +0100

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,12 @@ $result = $stmt->get_result();
 
 if ($result->num_rows === 1) {
   $user = $result->fetch_assoc();
+
   if (password_verify($password, $user['password'])) {
     $_SESSION['admin'] = $user['email'];
     echo "success";
-    exit
+    exit;
+  }
+}
+
+echo "error";
