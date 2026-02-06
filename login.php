516672410ec251fdc278c36a9227c284ce195fca
Mahamat Adam Mahamat <mamifsadam@gmail.com>
2026-02-06 11:14:16 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,26 +1,4 @@
 <?php
-include "config.php";
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$stmt = $conn->prepare("SELECT * FROM admins WHERE email=?");
-$stmt->bind_param("s", $email);
-$stmt->execute();
-$result = $stmt->get_result();
-
-if ($result->num_rows === 1) {
-  $user = $result->fetch_assoc();
-
-  if (password_verify($password, $user['password'])) {
-    $_SESSION['admin'] = $user['email'];
-    echo "success";
-    exit;
-  }
-}
-
-echo "error";
-<?php
 session_start();
 include "db.php";
 
