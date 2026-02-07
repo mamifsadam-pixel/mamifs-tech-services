1f8b0b59b7d0d4d9c1796115431c9e21283766ac
Mahamat Adam Mahamat <mamifsadam@gmail.com>
2026-02-07 22:10:06 +0100

    Update login.js

--- login.js
+++ login.js
@@ -5,3 +5,23 @@ lamp.addEventListener("click", () => {
   lamp.classList.toggle("on");
   panel.classList.toggle("show");
 });
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const data = new FormData();
+  data.append("email", email.value);
+  data.append("password", password.value);
+
+  fetch("login.php", {
+    method: "POST",
+    body: data
+  })
+  .then(res => res.text())
+  .then(res => {
+    if (res.trim() === "success") {
+      window.location = "dashboard.php";
+    } else {
+      document.getElementById("msg").innerText = "❌ Wrong login";
+    }
+  });
+});
